80966432346cd71850b27a3184c52caa2b395c94
memanot <menemty@gmail.com>
2021-09-18 16:14:42 +0300

    rename

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 import { extractProxy, ProxyList } from 'proxy-extract'
 
-class ProxyObserver {
+class ProxyObservator {
   public static proxies: ProxyList.IFreeProxy[] = []
   private static timerId: NodeJS.Timeout
 
@@ -10,32 +10,29 @@ class ProxyObserver {
       return
     }
 
-    clearTimeout(ProxyObserver.timerId)
-    ProxyObserver.timerId = setTimeout(
-      async () => await this.refresh(),
-      refreshTimeout
-    )
+    clearTimeout(ProxyObservator.timerId)
+    ProxyObservator.timerId = setTimeout(async () => await this.refresh(), refreshTimeout)
   }
 
   static async stop() {
-    clearTimeout(ProxyObserver.timerId)
-    ProxyObserver.proxies = []
+    clearTimeout(ProxyObservator.timerId)
+    ProxyObservator.proxies = []
   }
 
   static getRandom() {
-    return _.shuffle(ProxyObserver.proxies)[0]
+    return _.shuffle(ProxyObservator.proxies)[0]
   }
 
   static async refresh() {
     const newProxies = await extractProxy({
       count: 50,
-      tryLimit: 5,
+      tryLimit: 5
     })
 
     if (newProxies?.length) {
-      ProxyObserver.proxies = newProxies
+      ProxyObservator.proxies = newProxies
     }
   }
 }
 
-export { ProxyObserver }
+export { ProxyObservator }
