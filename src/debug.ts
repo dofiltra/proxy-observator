7d887e5150a38e2835d916edb182e74a64fe5987
memanot <menemty@gmail.com>
2021-09-18 20:09:51 +0300

    debug

--- src/debug.ts
+++ src/debug.ts
@@ -1,5 +1,12 @@
-// const startDebug = async () => {
-//   await ProxyObservator.start(1, 1, 3e3)
-//   await sleep(100e3)
-// }
-// startDebug()
+import { sleep } from 'time-helpers'
+import { ProxyObservator } from '.'
+
+const startDebug = async () => {
+  ProxyObservator.start({ count: 1, tryLimit: 1, refreshTimeout: 3e3 })
+
+  while (true) {
+    await sleep(5e3)
+    console.log(ProxyObservator.getAll())
+  }
+}
+startDebug()
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
 import _ from 'lodash'
 import { extractProxy, ProxyList } from 'proxy-extract'
 
+type TStartOpts = {
+  count: number
+  tryLimit?: number
+  refreshTimeout?: number
+}
+
 class ProxyObservator {
   public static proxies: ProxyList.IFreeProxy[] = []
   private static timerId: NodeJS.Timeout
 
-  static async start(count = 10, tryLimit = 1, refreshTimeout?: number) {
-    // console.log('refresh')
-    const newProxies = await extractProxy({
-      count,
-      tryLimit
-    })
-    // console.log('refresh', newProxies?.length)
-    ProxyObservator.proxies = newProxies?.length ? newProxies : []
+  static async start(opts: TStartOpts) {
+    const { count = 10, tryLimit = 1, refreshTimeout = 0 } = opts
 
-    if (refreshTimeout) {
-      //   clearTimeout(ProxyObservator.timerId)
-      ProxyObservator.timerId = setTimeout(() => ProxyObservator.start(count, tryLimit, refreshTimeout), refreshTimeout)
-    }
-    return ProxyObservator.timerId
+    return new Promise(async (resolve) => {
+      const newProxies = await extractProxy({
+        count,
+        tryLimit
+      })
+      ProxyObservator.proxies = newProxies?.length ? newProxies : []
+
+      if (refreshTimeout) {
+        // clearTimeout(ProxyObservator.timerId)
+        ProxyObservator.timerId = setTimeout(() => ProxyObservator.start(opts), refreshTimeout)
+      }
+
+      return resolve(ProxyObservator.timerId)
+    })
   }
 
   static async stop() {
